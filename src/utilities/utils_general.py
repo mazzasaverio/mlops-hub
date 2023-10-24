76e0b94be3b5893f867de2fdd9130a77a0070f4f
mazzasaverio <saverio3107@gmail.com>
2023-10-24 15:11:05 +0200

    UPDATE:notebook

--- src/utilities/utils_general.py
+++ src/utilities/utils_general.py
@@ -7,17 +7,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_project_directory():
-    current_directory = os.getcwd()
-    
-    # Define environment-specific directories
-    env_directories = {
-        '/content': '/content/',
-        '/kaggle/working': '/kaggle/'
-    }
-    
-    return env_directories.get(current_directory, os.getenv("ROOT_PATH"))
-
 
 def load_config(path: str) -> dict:
     try:
@@ -34,12 +23,3 @@ def load_dataset(path: str) -> pd.DataFrame:
         print(f"Data file {path} not found. Exiting.")
         sys.exit()
 
-
-
-
-def prepare_features_and_target(df_train: pd.DataFrame) -> (pd.DataFrame, pd.Series):
-    cols_feat = [c for c in df_train.columns if c not in ['datetime', 'target', 'ticker']]
-    X = df_train[cols_feat]
-    y = df_train['target']
-    y = y.map({-1: 0, 0: 1, 1: 2})
-    return X, y
